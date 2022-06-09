d5bafb1d990b3226cf68b49226648bf7f1985cd9
wongfaye309 <wongfaye309@gmail.com>
2022-06-09 15:54:23 +0800

    20220609

---
+++ src/main/java/com/example/springmvc20220609/Springmvc20220609Application.java
@@ -0,0 +1,13 @@
+package com.example.springmvc20220609;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springmvc20220609Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springmvc20220609Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springmvc20220609/Springmvc20220609ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmvc20220609;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springmvc20220609ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
